5e7324cc3d3e221ccfbd59f9a59dc03e82dd4dc5
Rochronaldo-byte <roch4619@gmail.com>
2025-09-09 19:43:06 +0530

    Initial commit ofOpenRMF-Introduction

---
+++ fleet_adapter/setup.py
@@ -0,0 +1,36 @@
+from glob import glob
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+package_name = 'fleet_adapter'
+
+setup(
+    name=package_name,
+    version='2.8.0',
+    packages=find_packages(),
+    data_files=[
+        (
+            'share/ament_index/resource_index/packages',
+            ['resource/' + package_name],
+        ),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.xml')),
+    ],
+    install_requires=['setuptools', 'fastapi>=0.79.0', 'uvicorn>=0.18.2'],
+    zip_safe=True,
+    maintainer='Roch Ronaldo',
+    maintainer_email='roch4619@gmail.com',
+    description='Fleet adapters for interfacing with RMF Demos robots with a '
+    'fleet manager via REST API',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'fleet_adapter=fleet_adapter.fleet_adapter:main',
+            'fleet_manager=fleet_adapter.fleet_manager:main',
+        ],
+    },
+)
